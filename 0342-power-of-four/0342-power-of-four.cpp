676eed1b0283063e9bc2b4095efc95d9ee743bd2
Mohit Borekar <136325923+Mohit-Borekar@users.noreply.github.com>
2026-08-05 11:47:06 +0530

    Time: 3 ms (2.89%), Space: 7.9 MB (49.76%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n<=0){
+            return 0;
+        }
+        while(n%4 == 0){
+            n = n / 4;
+            if(n == 1){
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
